623a80b48c9112c6bf4b859711c2a6d8752cfe10
Anup <anup_thapa@outlook.com>
2017-06-06 04:29:30 +0000

    s

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -2,24 +2,25 @@ class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
+    attr_reader attr_name + "_history" # create bar_history getter
     class_eval %Q{
-      def #{attr_name}=(val)
+      def #{attr_name}=(new_val)
         if @#{attr_name}_history.nil?
           @#{attr_name}_history = [nil]
         end
-        @#{attr_name}_history.push(val)
-        @#{attr_name}=val
+        @#{attr_name}_history.push(new_val)
+        @#{attr_name}=new_val
       end
     }
   end
 end
-class Check
+class Foo
     attr_accessor_with_history :bar
 end
 
-f = Check.new     
-puts f.bar = 7      
-puts f.bar = :HalaMadrid!  
-puts f.bar = 'Vamos!' 
-puts f.bar_history 
+f1 = Foo.new
+f2 = Foo.new
+f1.bar = 3 ; f1.bar = 4      
+f2.bar = :x ; f2.bar = :y  
+puts f1.bar_history
+puts f2.bar_history 
